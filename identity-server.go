4d5e14bac52245027a0158e8107e3e40013317e8
Bryan Stenson <bryan.stenson@gmail.com>
2014-06-01 12:53:12 -0700

    returning json token object with expiration

--- identity-server.go
+++ identity-server.go
@@ -2,10 +2,35 @@ package main
 
 import (
   "fmt"
+  "time"
+  "encoding/json"
+  "net/http"
   "github.com/dchest/uniuri"
 )
 
 func main() {
-  fmt.Printf(uniuri.NewLen(256))
-  fmt.Printf("\n")
+  http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    s, _ := json.Marshal(TokenGenerator())
+    fmt.Fprint(w, string(s))
+    return
+  })
+
+  http.ListenAndServe(":8080", nil)
+}
+
+const Token_Length = 256
+const Token_Duration = time.Hour
+
+type Token struct {
+  Id string
+  Expiration time.Time
+}
+
+func TokenGenerator() Token {
+  answer := Token{
+    Id : uniuri.NewLen(Token_Length),
+    Expiration: time.Now().Add(Token_Duration).UTC(),
+  }
+  return answer
 }
